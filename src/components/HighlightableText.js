77f2873ad0cd910c7667ab7f38cb5b9e6d361977
Rainer Bachleitner <r.bachleitner@gmail.com>
2017-07-31 12:35:59 +0200

    Highlightable Text component missing from previous commit

---
+++ src/components/HighlightableText.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+function highlightPartsOfString(highlightStrings, value) {
+  let indices = [];
+  const value_ = value.toLowerCase();
+  highlightStrings.forEach((string) => {
+    string = string.toLowerCase();
+    if (string !== '' && value_.includes(string)) {
+      const startIndex = value_.indexOf(string);
+      const endIndex = startIndex + string.length;
+      indices.push([startIndex, endIndex]);
+    }
+  });
+  let returnString = [];
+  let lastIndex = 0;
+  indices.forEach((index) => {
+    if (index[0] >= lastIndex) {
+      returnString.push([value.slice(lastIndex, index[0]), false]);
+      returnString.push([value.slice(index[0], index[1]), true]);
+      lastIndex = index[1];
+    }
+  });
+  if (lastIndex < value.length - 1) returnString.push([value.slice(lastIndex, value.length - 1), false]);
+  return returnString;
+}
+
+const HighlightableText =
+  observer(
+    ({highlightStrings, value}) =>{
+      const stringList = highlightPartsOfString(highlightStrings, value);
+      return <span>{stringList.map((entry) => {
+        return entry[1] ? <span style={{backgroundColor: 'yellow'}}>{entry[0]}</span> : entry[0]
+      })}</span>
+    });
+
+export default HighlightableText;
