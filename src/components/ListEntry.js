7b93d06f207f2cbb4832296c9cdfe29f3c617c2f
Rainer Bachleitner <r.bachleitner@gmail.com>
2017-08-03 13:44:45 +0200

    Add preview pdf to enlarged list entry

--- src/components/ListEntry.js
+++ src/components/ListEntry.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PDFModal from './PDFModal';
+import PDFPreviewModal from './PDFPreviewModal';
 import HighlightableText from './HighlightableText';
 import { observer } from 'mobx-react';
 import { onListClick } from '../models/ListEvents';
@@ -12,6 +13,7 @@ const ListEntry =
     ({store, paper}) => {
 
       const abstract = paper.clicked ? paper.paper_abstract : paper.paper_abstract.slice(0, 300) + '...';
+      const listEntryClassName = paper.clicked ? "list_entry_full" : "list_entry";
       const openAccessLogoStyle = paper.oa ? {display: 'inline'} : {display: 'none'};
       const authors = (paper.authors);
       const htmlLink = <Button bsStyle="info" bsSize="xsmall" href={paper.oa_link} target="_blank"> HTML <span className="outlink_symbol">&#xf08e;</span></Button>;
@@ -25,47 +27,53 @@ const ListEntry =
             <HighlightableText highlightStrings={highlightStrings} value={paper.title}/></a>
         </div>;
 
+      const preview = paper.clicked ?
+        <PDFPreviewModal link={paper.oa_link_pdf}/> : '';
+
       return (
-        <div className="list_entry">
+        <div id="list_holder">
+          <div className={listEntryClassName}>
 
-          <div className="list_metadata">
+            <div className="list_metadata">
 
-            {titleLink}
+              {titleLink}
 
-            <div className="list_links">
-              {htmlLink}
-              {pdfLink}
+              <div className="list_links">
+                {htmlLink}
+                {pdfLink}
+              </div>
+
+              <div className="list_details highlightable">
+                <span className="list_authors">
+                  <HighlightableText highlightStrings={highlightStrings} value={authors} />
+                  </span>
+                <span className="list_in"> in </span>
+                <span className="list_published_in">{paper.published_in}</span>
+                <span className="list_pubyear">
+                  (<HighlightableText highlightStrings={highlightStrings} value={paper.year} />)
+                </span>
+              </div>
+
             </div>
 
-            <div className="list_details highlightable">
-              <span className="list_authors">
-                <HighlightableText highlightStrings={highlightStrings} value={authors} />
-                </span>
-              <span className="list_in"> in </span>
-              <span className="list_published_in">{paper.published_in}</span>
-              <span className="list_pubyear">
-                (<HighlightableText highlightStrings={highlightStrings} value={paper.year} />)
+            <div className="highlightable">
+              <p id="list_abstract">
+                  <HighlightableText highlightStrings={highlightStrings} value={abstract} />
+                </p>
+            </div>
+
+            <div id="list_area">
+              <span className="area_tag">Area:</span> <span className="area_name">
+                <HighlightableText highlightStrings={highlightStrings} value={paper.area}/>
               </span>
             </div>
 
-          </div>
+            <div className="list_readers">
+              <span className="num_readers">{paper.readers}</span> <span className="list_readers_entity">readers</span>&nbsp;
+            </div>
 
-          <div className="highlightable">
-            <p id="list_abstract">
-                <HighlightableText highlightStrings={highlightStrings} value={abstract} />
-              </p>
-          </div>
-
-          <div id="list_area">
-            <span className="area_tag">Area:</span> <span className="area_name">
-              <HighlightableText highlightStrings={highlightStrings} value={paper.area}/>
-            </span>
-          </div>
-
-          <div className="list_readers">
-            <span className="num_readers">{paper.readers}</span> <span className="list_readers_entity">readers</span>&nbsp;
-          </div>
-
+        </div>
+          {preview}
       </div>
         );
     }
---
+++ src/components/PDFPreviewModal.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {Button, Modal} from 'react-bootstrap';
+
+const PDFPreviewModal = class PDFPreviewModal extends React.Component
+{
+  constructor()
+  {
+    super();
+    this.state = {
+      showModal: false,
+      showTransboxText: 'none'
+    }
+  }
+
+  showClickText() {
+    this.setState({showTransboxText : 'block'})
+  }
+
+  hideClickText() {
+    this.setState({showTransboxText : 'none'})
+  }
+
+  close()
+  {
+    this.setState({showModal: false});
+  }
+
+  open()
+  {
+    this.setState({showModal: true});
+  }
+
+  render()
+  {
+    let displayTransbox = 'none';
+    let viewer = process.env.PUBLIC_URL + "/pdfjs-hypothesis/web/viewer.html";
+    viewer = viewer + "?file=" + process.env.PUBLIC_URL + '/mockpdf.pdf';
+    return (
+      <div style={{display: "block"}}>
+        <div
+          id="preview_image"
+          style={{width: "230px", height: "298px", background: 'url("//openknowledgemaps.org/search_api/dist/img/preview_pdf.png") 0% 0% / 230px,  0% 0% / 298px'}}
+          onClick={this.open.bind(this)}
+          onMouseEnter={this.showClickText.bind(this)}
+          onMouseLeave={this.hideClickText.bind(this)}
+          >
+          <div id="transbox" style={{width: "230px", height: "298px", display: this.state.showTransboxText}}>
+            Click here to open preview
+          </div>
+        </div>
+
+
+        <Modal show={this.state.showModal} onHide={this.close.bind(this)} bsSize="large" aria-labelledby="contained-modal-title-lg">
+          <Modal.Header closeButton>
+          </Modal.Header>
+          <Modal.Body style={{width: "100%"}}>
+            {/*<span id="spinner-iframe" className="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>*/}
+            <iframe id="pdf_iframe" className="block" src={viewer} frameBorder="0" style={{width: "100%", height: window.innerHeight - 200}} title="PDF"></iframe>
+            {/*<div id="status"></div>*/}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close.bind(this)}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+};
+
+export default PDFPreviewModal;
