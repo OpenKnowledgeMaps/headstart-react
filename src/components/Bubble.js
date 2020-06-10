263987b491437596a4de313c4c080fe1f264f6b3
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-06-10 13:56:49 +0200

    ANIMATIONLESS DEMO

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -9,81 +9,88 @@ import {onBubbleMouseEnter, onBubbleMouseLeave, onBubbleClick, onBubbleDoubleCli
  * Binds eventlisteners to bubbles
  * @type {<T extends IReactComponent>(clazz: T) => void | IReactComponent | (function({node?: *, store?: *}))}
  */
-const Bubble =
-  observer(
-    ({node, store}) => {
-      // TODO remove all magical numbers and make styling clearer through classes/external definitions
-      /** TODO
-       * Inline styles in the render function should be either
-       * a) extracted and incorporated in the SASS stylesheets (src/stylesheets)
-       * b) extracted to a Javascript Object that belongs specifically to this
-       *    Component. e.g. let infoModalStyles = { div: { margin: "0 0 30px" } }
-       *    which we could use like <div style={infoModalStyles.div}> ... </div>
-       */
-      let circleClassName = null;
-      let circleStyle = {fillOpacity: "0.8"};
-      if (store.bubblesStore.hasSelectedEntities) {
-        circleClassName = (node.selected) ? "zoom_selected" : "zoom_unselected";
-        circleStyle.fillOpacity = (node.selected) ? "1." : "0.1";
-      } else {
-        circleClassName = node.active ? "zoom_selected" : "area";
-        circleStyle.fillOpacity = (node.active || node.selected) ? "1." : "0.8";
-      }
-      const {orig_x, orig_y, orig_r} = node;
-      const {zoomFactor, translationVecX, translationVecY} = store;
+class Bubble extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-      const x_ = zoomFactor * orig_x + translationVecX;
-      const y_ = zoomFactor * orig_y + translationVecY;
-      const r_ = zoomFactor * orig_r;
+  getCoordinates() {
+    const { orig_x, orig_y, orig_r } = this.props.node;
+    const { zoomFactor, translationVecX, translationVecY } = this.props.store;
+  
+    const x_ = zoomFactor * orig_x + translationVecX;
+    const y_ = zoomFactor * orig_y + translationVecY;
+    const r_ = zoomFactor * orig_r;
+  
+    return { x_, y_, r_ };
+  };
+    
 
-      const sqrtOfTwo = Math.sqrt(2);
-      let areaTitleStyle = {wordWrap : "break-word", fontSize : "12px", width: 2*r_/sqrtOfTwo, height: 2*r_/sqrtOfTwo};
-      if ((node.active || node.selected) || (store.bubblesStore.hasSelectedEntities && !node.selected)) {
-        areaTitleStyle.display = "none";
-      }
-      
-      let titleFontSize = "16px";
-      if (store.svgWidth <= 650) {
-        titleFontSize = "12px";
-      } else if (store.svgWidth <= 1050) {
-        titleFontSize = "14px";
-      }
+  render() {
+    let node = this.props.node;
+    let store = this.props.store;
 
-      const highlightStrings = store.searchString.split(' ');
-
-      const areaName = (node.area.length > 55) ? node.area.slice(0,55) + "..." : node.area;
-      return (
-        <g onMouseEnter={onBubbleMouseEnter.bind(this, store, node)}
-           onMouseLeave={onBubbleMouseLeave.bind(this, store)}
-           onClick={onBubbleClick.bind(this, store, node)}
-           onDoubleClick={onBubbleDoubleClick.bind(this, store, node)}
-           className="bubble_frame"
-        >
-          <circle
-            className={circleClassName}
-            r={r_}
-            cx={x_}
-            cy={y_}
-            style={circleStyle}
-          />
-          <foreignObject
-            x={x_ - r_/sqrtOfTwo}
-            y={y_ - r_/sqrtOfTwo}
-            width={2*r_/sqrtOfTwo}
-            height={2*r_/sqrtOfTwo}
-            id="area_title_object"
-            className="headstart"
-          >
-            <div className="outerDiv">
-              <div id="area_title" style={areaTitleStyle} className="innerDiv">
-                  <h2 className="highlightable" style={{fontSize: titleFontSize}}>
-                    <HighlightableText highlightStrings={highlightStrings} value={areaName} />
-                    </h2>
-              </div>
-            </div>
-          </foreignObject>
-        </g>
-      );
+    let circleClassName = null;
+    let circleStyle = {fillOpacity: "0.8"};
+    if (store.bubblesStore.hasSelectedEntities) {
+      circleClassName = (node.selected) ? "zoom_selected" : "zoom_unselected";
+      circleStyle.fillOpacity = (node.selected) ? "1." : "0.1";
+    } else {
+      circleClassName = node.active ? "zoom_selected" : "area";
+      circleStyle.fillOpacity = (node.active || node.selected) ? "1." : "0.8";
     }
-  );
-export default Bubble;
+    
+    const { x_, y_, r_ } = this.getCoordinates();
+
+    const sqrtOfTwo = Math.sqrt(2);
+    let areaTitleStyle = {wordWrap : "break-word", fontSize : "12px", width: 2*r_/sqrtOfTwo, height: 2*r_/sqrtOfTwo};
+    if ((node.active || node.selected) || (store.bubblesStore.hasSelectedEntities && !node.selected)) {
+      areaTitleStyle.display = "none";
+    }
+    
+    let titleFontSize = "16px";
+    if (store.svgWidth <= 650) {
+      titleFontSize = "12px";
+    } else if (store.svgWidth <= 1050) {
+      titleFontSize = "14px";
+    }
+
+    const highlightStrings = store.searchString.split(' ');
+
+    const areaName = (node.area.length > 55) ? node.area.slice(0,55) + "..." : node.area;
+    return (
+      <g onMouseEnter={onBubbleMouseEnter.bind(this, store, node)}
+          onMouseLeave={onBubbleMouseLeave.bind(this, store)}
+          onClick={onBubbleClick.bind(this, store, node)}
+          onDoubleClick={onBubbleDoubleClick.bind(this, store, node)}
+          className="bubble_frame"
+      >
+        <circle
+          className={circleClassName}
+          r={r_}
+          cx={x_}
+          cy={y_}
+          style={circleStyle}
+        />
+        <foreignObject
+          x={x_ - r_/sqrtOfTwo}
+          y={y_ - r_/sqrtOfTwo}
+          width={2 * r_ / sqrtOfTwo}
+          height={2 * r_ / sqrtOfTwo}
+          id="area_title_object"
+          className="headstart"
+        >
+          <div className="outerDiv">
+            <div id="area_title" style={areaTitleStyle} className="innerDiv">
+                <h2 className="highlightable" style={{fontSize: titleFontSize}}>
+                  <HighlightableText highlightStrings={highlightStrings} value={areaName} />
+                  </h2>
+            </div>
+          </div>
+        </foreignObject>
+      </g>
+    );
+  }
+}
+
+export default observer(Bubble);
